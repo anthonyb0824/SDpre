00b67beb1344c154886c4efab60cce2891363a0e
anthonyb0824 <anthonybutler0824@gmail.com>
2021-12-14 20:17:25 -0600

    objective2 Labs

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Anthony Butler";
+    int age = 24;
+    String state = "Texas";
+    System.out.println("Name: " + name + " Age: " + age +  " State: " + state);
+  }
+}
